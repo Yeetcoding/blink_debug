c8ab6dc79eb1c1e90f3ad316e826d91f6f1b6b83
Steve Cline <steve.cline@weldre4.org>
2021-01-27 08:21:57 -0700

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,12 @@
+// make a light blink every one second on and off
+
+void setup()
+{
+    pinMode (6, HIGH);
+    digitalWrite (6, HIGH);
+    delay (1000)
+    digitalWrite (6, LOW);
+
+}
+
+delay (2000);
